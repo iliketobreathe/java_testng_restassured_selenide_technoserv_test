135338a38caa5b2ae7930ae316e5af2586f467cf
Aleksandr Mukhamedov <3a2nok@gmail.com>
2020-04-22 03:30:17 +0300

    Initial commit

---
+++ src/main/java/UserAddTest.java
@@ -0,0 +1,12 @@
+public class UserAddTest {
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+}
---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,46 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ApiTests {
+
+    @Test
+    public void getUsersTest() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        List<UserGetTest> users = Arrays.asList(response.jsonPath().getObject("data", UserGetTest[].class));
+        for (UserGetTest user : users) {
+            Assert.assertNotNull(user.getFirst_name());
+            Assert.assertNotNull(user.getLast_name());
+            Assert.assertNotNull(user.getEmail());
+            Assert.assertNotNull(user.getAvatar());
+        }
+    }
+
+    @Test
+    public void createUserTest() {
+        LinkedHashMap<String, String> requestBody = new LinkedHashMap<String, String>();
+        requestBody.put("name", "morpheus");
+        requestBody.put("job", "leader");
+
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody);
+
+        Response response = request.post("https://reqres.in/api/users");
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+
+        UserAddTest user = response.as(UserAddTest.class);
+
+        Assert.assertEquals(user.getName(), "morpheus");
+        Assert.assertEquals(user.getJob(), "leader");
+    }
+}
